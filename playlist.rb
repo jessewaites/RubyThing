d628f75788b8dc7b2312048191a5df8712a4e3df
Jesse Waites <jesse.waites@gmail.com>
2013-09-21 00:31:14 -0500

    refactored methods into module

--- playlist.rb
+++ playlist.rb
@@ -1,4 +1,6 @@
 require_relative 'movie'
+require_relative 'reviewer'
+
 
 class Playlist
 	def initialize(name)
@@ -10,28 +12,14 @@ class Playlist
 		@movies << movie
 	end
 
-	def roll_die
-		rand(1..6)
-	end	
 	
 	def play	
 	puts "#{@name}'s playlist:"
-	puts			 
+	puts @movies			 
 
-	@movies.each do |m|
-		roll_die
-		number_rolled = roll_die
-		case number_rolled
-		when 1..2
-			m.thumbs_down
-			puts "#{m.title} got a thumbs down."
-		when 3..4
-			puts "#{m.title} was skipped."
-		else
-			m.thumbs_up
-			puts "#{m.title} got a thumbs up."
-		end		
-	puts m	
+	@movies.each do |movie|
+		Reviewer.review(movie)
+	puts movie	
 	end
 	puts
 	end
